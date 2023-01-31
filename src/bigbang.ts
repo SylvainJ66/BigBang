8b9c63d1778a5adf538db08a85f32e3e0a065683
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-01-31 13:03:34 +0100

    delete existing to just keep stater project

--- src/bigbang.ts
+++
@@ -1,9 +0,0 @@
-class BigBang {
-  Parse(number: number) {
-    if (number % 7 === 0) return 'bang';
-    if (number % 5 === 0) return 'big';
-    return number.toString();
-  }
-}
-
-export default new BigBang();
