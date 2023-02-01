a2f58ba36ee7215fb0df88862232fdec6d2a02bd
SylvainJ66 <sylvain.jouglard.66@gmail.com>
2023-02-01 23:58:34 +0100

    run tests

--- src/bigbang.ts
+++ src/bigbang.ts
@@ -1,5 +1,6 @@
 class BigBang {
   parse(number: number) {
+    if(number === 11) return 'boom';
     if(number % 7 === 0) return 'bang';
     if(number % 5 === 0) return 'big';
     return number.toString();
