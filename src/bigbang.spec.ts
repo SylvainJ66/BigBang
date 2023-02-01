f072000bd169383902c306a93a39f93ec571da4e
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-01 15:53:06 +0100

    reload to improve refactor

--- src/bigbang.spec.ts
+++ src/bigbang.spec.ts
@@ -1,76 +1,7 @@
 import bigbang from './bigbang';
 
 describe('BigBang', () => {
-  test('should parse 1 to "1"', () => {
-    expect(bigbang.parse(1)).toBe('1');
-  });
-  test('should parse 2 to "2"', () => {
-    expect(bigbang.parse(2)).toBe('2');
-  });
-  test('should parse 3 to "3"', () => {
-    expect(bigbang.parse(3)).toBe('3');
-  });
-  test('should parse 5 to "big"', () => {
-    expect(bigbang.parse(5)).toBe('big');
-  });
-  test('should parse 10 to "big"', () => {
-    expect(bigbang.parse(10)).toBe('big');
-  });
-  test('should parse 15 to "big"', () => {
-    expect(bigbang.parse(15)).toBe('big');
-  });
-  test('should parse 7 to "bang"', () => {
-    expect(bigbang.parse(7)).toBe('bang');
-  });
-  test('should parse 14 to "bang"', () => {
-    expect(bigbang.parse(14)).toBe('bang');
-  });
-  test('should parse 21 to "bang"', () => {
-    expect(bigbang.parse(21)).toBe('bang');
-  });
-  test('should parse 35 to "bigbang"', () => {
-    expect(bigbang.parse(35)).toBe('bigbang');
-  });
-  test('should parse 105 to "bigbang"', () => {
-    expect(bigbang.parse(105)).toBe('bigbang');
-  });
-  test('should parse 70 to "bangbig"', () => {
-    expect(bigbang.parse(70)).toBe('bangbig');
-  });
-  test('should parse 140 to "bangbig"', () => {
-    expect(bigbang.parse(140)).toBe('bangbig');
-  });
-  test('should parse 11 to "boom"', () => {
-    expect(bigbang.parse(11)).toBe('boom');
-  });
-  test('should parse 22 to "boom"', () => {
-    expect(bigbang.parse(22)).toBe('boom');
-  });
-  test('should parse 33 to "boom"', () => {
-    expect(bigbang.parse(33)).toBe('boom');
-  });
-  test('should parse 55 to "bigboom"', () => {
-    expect(bigbang.parse(55)).toBe('bigboom');
-  });
-  test('should parse 165 to "bigboom"', () => {
-    expect(bigbang.parse(165)).toBe('bigboom');
-  });
-  test('should parse 110 to "boombig"', () => {
-    expect(bigbang.parse(110)).toBe('boombig');
-  });
-  test('should parse 220 to "boombig"', () => {
-    expect(bigbang.parse(220)).toBe('boombig');
-  });
-  test('should parse 385 () to "bigbangboom"', () => {
-    expect(bigbang.parse(385)).toBe('bigbangboom');
-  });
-  test('should parse 1155 to "bigbangboom"', () => {
-    expect(bigbang.parse(1155)).toBe('bigbangboom');
-  });
-  test('should parse 1540 to "boombigbang"', () => {
-    expect(bigbang.parse(1540)).toBe('boombigbang');
-  });
-  test('should parse 234411 to "234411"', () => {
-    expect(bigbang.parse(234411)).toBe('234411');
+  test('should ', () => {
+    expect().toBe();
   });
 });
--- src/bigbang.ts
+++ src/bigbang.ts
@@ -1,16 +1,4 @@
 class BigBang {
-  parse(number: number) {
-    if (number % (5 * 7 * 11 * 2) === 0) return 'boombigbang';
-    if (number % (5 * 7 * 11) === 0) return 'bigbangboom';
-    if (number % (5 * 11 * 2) === 0) return 'boombig';
-    if (number % (5 * 11) === 0) return 'bigboom';
-    if (number % (5 * 7 * 2) === 0) return 'bangbig';
-    if (number % (5 * 7) === 0) return 'bigbang';
-    if (number % 11 === 0) return 'boom';
-    if (number % 7 === 0) return 'bang';
-    if (number % 5 === 0) return 'big';
-    return number.toString();
-  }
 }
 
 export default new BigBang();
