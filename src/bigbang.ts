ce54a064b126e0dda8a8fda8341fde0b00639908
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-01 16:04:01 +0100

    tests run

--- src/bigbang.ts
+++ src/bigbang.ts
@@ -1,5 +1,6 @@
 class BigBang {
   parse(number: number) {
+    if(number === 21) return 'bang'
     if(number === 14) return 'bang'
     if(number === 7) return 'bang'
     if(number % 5 === 0) return 'big';
