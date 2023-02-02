0fae85c81390f8d2dbd426e0b5d1b273534fc67e
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-02 08:43:31 +0100

    tests run

--- src/bigbang.spec.ts
+++ src/bigbang.spec.ts
@@ -19,4 +19,25 @@ describe('BigBang', () => {
   test('should parse 15 to "big"', () => {
     expect(bigbang.parse(15)).toBe('big');
   });
+  test('should parse 7 to "bang"', () => {
+    expect(bigbang.parse(7)).toBe('bang');
+  });
+  test('should parse 14 to "bang"', () => {
+    expect(bigbang.parse(14)).toBe('bang');
+  });
+  test('should parse 11 to "boom"', () => {
+    expect(bigbang.parse(11)).toBe('boom');
+  });
+  test('should parse 22 to "boom"', () => {
+    expect(bigbang.parse(22)).toBe('boom');
+  });
+  test('should parse 5*7 to "bigbang"', () => {
+    expect(bigbang.parse(35)).toBe('bigbang');
+  });
+  test('should parse 5*7*3 to "bigbang"', () => {
+    expect(bigbang.parse(105)).toBe('bigbang');
+  });
+  test('should parse 5*7*2 to "bangbig"', () => {
+    expect(bigbang.parse(105)).toBe('bangbig');
+  });
 });
