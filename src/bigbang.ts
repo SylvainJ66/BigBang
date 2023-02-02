929b5d0e7c41bd6fb8f826ccbafd1b37848595fd
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-02 08:49:53 +0100

    tests run

--- src/bigbang.ts
+++ src/bigbang.ts
@@ -1,11 +1,11 @@
 class BigBang {
   parse(number: number) {
     let result = number.toString();
-    if (this.multipleOf(number, 5 * 7 * 2)) result = 'bangbig';
-    if (this.multipleOf(number, 5 * 7)) result = 'bigbang';
     if (this.multipleOf(number, 11)) result = 'boom';
     if (this.multipleOf(number, 7)) result = 'bang';
     if (this.multipleOf(number, 5)) result = 'big';
+    if (this.multipleOf(number, 5 * 7)) result = 'bigbang';
+    if (this.multipleOf(number, 5 * 7 * 2)) result = 'bangbig';
     return result;
   }
 
